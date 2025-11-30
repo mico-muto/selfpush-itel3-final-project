f927a70cd659a7087f42da67a2bec8ab39a01815
bryanpaolomangabat-ux <bryanpaolo.mangabat@neu.edu.ph>
2025-11-30 23:30:35 +0800

    Add Swagger documentation and update server.js

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Music Playlist CRUD API',
+    version: '1.0.0',
+    description: 'Swagger documentation for the Music Playlist API',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./server.js'],
+};
+
+module.exports = swaggerJSDoc(options);
